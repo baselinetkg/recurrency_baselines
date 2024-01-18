1d6f73e1f6656547f2202f15bd02df486905dc8b
baselinetkg <baselinetkg@gmail.com>
2024-01-18 11:33:20 +0100

    modify methods for evaluation

--- src/evaluation/run_evaluation.py
+++ src/evaluation/run_evaluation.py
@@ -1,42 +1,3 @@
-"""*
- *     Reccurency Baselines 
- *
- *        File: run_evaluation.py
- *
- *     Authors: Deleted for purposes of anonymity 
- *
- *     Proprietor: Deleted for purposes of anonymity --- PROPRIETARY INFORMATION
- * 
- * The software and its source code contain valuable trade secrets and shall be maintained in
- * confidence and treated as confidential information. The software may only be used for 
- * evaluation and/or testing purposes, unless otherwise explicitly stated in the terms of a
- * license agreement or nondisclosure agreement with the proprietor of the software. 
- * Any unauthorized publication, transfer to third parties, or duplication of the object or
- * source code---either totally or in part---is strictly prohibited.
- *
- *     Copyright (c) 2021 Proprietor: Deleted for purposes of anonymity
- *     All Rights Reserved.
- *
- * THE PROPRIETOR DISCLAIMS ALL WARRANTIES, EITHER EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY 
- * AND FITNESS FOR A PARTICULAR PURPOSE AND THE WARRANTY AGAINST LATENT 
- * DEFECTS, WITH RESPECT TO THE PROGRAM AND ANY ACCOMPANYING DOCUMENTATION. 
- * 
- * NO LIABILITY FOR CONSEQUENTIAL DAMAGES:
- * IN NO EVENT SHALL THE PROPRIETOR OR ANY OF ITS SUBSIDIARIES BE 
- * LIABLE FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES
- * FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION, LOSS OF INFORMATION, OR
- * OTHER PECUNIARY LOSS AND INDIRECT, CONSEQUENTIAL, INCIDENTAL,
- * ECONOMIC OR PUNITIVE DAMAGES) ARISING OUT OF THE USE OF OR INABILITY
- * TO USE THIS PROGRAM, EVEN IF the proprietor HAS BEEN ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGES.
- * 
- * For purposes of anonymity, the identity of the proprietor is not given herewith. 
- * The identity of the proprietor will be given once the review of the 
- * conference submission is completed. 
- *
- * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
- *"""
 """
  compute results (mrr, hits, mrr per snapshot, mrr per rel, static, timeaware raw setting) and write to one output.json
  based on prediction scores that have been stored in pkl files (one per per method per dataset per config) in 
@@ -143,8 +104,11 @@ def setup(dataset_name: str, pickle_file: dict):
 
 
 if __name__ == '__main__':
-    dir_names = ['results/baselinepsi', 'results/baselinepsibaselinexi']
+    dir_names = ['results/baselinepsibaselinexi']
+    
     print('starting evaluation')
+    print('looking at the following directories: ', dir_names)
+    print('if you want to check additional baselines please enter in list dir_names')
 
     output_filename = 'output_final.json'
     summary_filename = 'summary_final.json'
